85ff1001cfaf3c89107ac2a667dbeb4b8e611dbc
wwan13 <wwan13@naver.com>
2022-10-13 00:10:57 +0900

    [feat]: initialize spring-boot project

---
+++ inflearn-rest/src/main/java/com/wwan13/inflearnrest/InflearnRestApplication.java
@@ -0,0 +1,13 @@
+package com.wwan13.inflearnrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnRestApplication.class, args);
+	}
+
+}
