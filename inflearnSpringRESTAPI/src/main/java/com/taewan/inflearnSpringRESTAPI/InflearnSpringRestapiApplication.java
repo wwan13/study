1e34db01ee6b8ce56c2a1554b4e9a2f01f417d09
wwan13 <wwan13@naver.com>
2022-10-18 00:25:29 +0900

    [feat]: initialize new spring projet

---
+++ inflearnSpringRESTAPI/src/main/java/com/taewan/inflearnSpringRESTAPI/InflearnSpringRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.taewan.inflearnSpringRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringRestapiApplication.class, args);
+	}
+
+}
---
+++ inflearnSpringRESTAPI/src/test/java/com/taewan/inflearnSpringRESTAPI/InflearnSpringRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taewan.inflearnSpringRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
