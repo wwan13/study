a3469c8e03eb704c0d72d8af360af92baf7a4387
wwan13 <wwan13@naver.com>
2022-11-03 23:41:06 +0900

    [feat]: fail on unknown properties

--- inflearnSpringRESTAPI/src/test/java/com/taewan/inflearnSpringRESTAPI/events/EventControllerTests.java
+++ inflearnSpringRESTAPI/src/test/java/com/taewan/inflearnSpringRESTAPI/events/EventControllerTests.java
@@ -35,6 +35,35 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("Rest Api Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022,11,23,14,00))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022,11,24,14,00))
+                .beginEventDateTime(LocalDateTime.of(2022,11,25,14,00))
+                .endEventDateTime(LocalDateTime.of(2022,11,26,14,00))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaTypes.HAL_JSON)
+                                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.PUBLISHED)));
+    }
+
+    @Test
+    public void createEvent_BadRequest() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -52,17 +81,11 @@ public class EventControllerTests {
                 .build();
 
         mockMvc.perform(post("/api/events/")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .accept(MediaTypes.HAL_JSON)
-                                .content(objectMapper.writeValueAsString(event)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.PUBLISHED)));
+                .andExpect(status().isBadRequest());
     }
 
 }
