9386c7ca3278204ff837c1a01d0906968e1c70e4
wwan13 <wwan13@naver.com>
2022-11-03 23:08:26 +0900

    [feat]: restrict input for POST /api/events

--- inflearnSpringRESTAPI/src/main/java/com/taewan/inflearnSpringRESTAPI/events/EventController.java
+++ inflearnSpringRESTAPI/src/main/java/com/taewan/inflearnSpringRESTAPI/events/EventController.java
@@ -1,5 +1,6 @@
 package com.taewan.inflearnSpringRESTAPI.events;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -18,12 +19,16 @@ public class EventController {
 
     private final EventRepository eventRepository;
 
-    public EventController(EventRepository eventRepository) {
+    private final ModelMapper modelMapper;
+
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper) {
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event) {
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto) {
+        Event event = modelMapper.map(eventDto, Event.class);
         Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
         event.setId(10);
--- inflearnSpringRESTAPI/src/main/java/com/taewan/inflearnSpringRESTAPI/events/EventDto.java
+++ inflearnSpringRESTAPI/src/main/java/com/taewan/inflearnSpringRESTAPI/events/EventDto.java
@@ -1,17 +1,16 @@
 package com.taewan.inflearnSpringRESTAPI.events;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Builder @AllArgsConstructor @NoArgsConstructor
-@Getter @Setter @EqualsAndHashCode(of= "id")
-@Entity
-public class Event {
+@Builder @NoArgsConstructor @AllArgsConstructor
+@Data
+public class EventDto {
 
-    @Id @GeneratedValue
-    private int id;
     private String name;
     private String description;
     private LocalDateTime beginEnrollmentDateTime;
@@ -22,9 +21,5 @@ public class Event {
     private int basePrice; // (optional)
     private int maxPrice; // (optional)
     private int limitOfEnrollment;
-    private boolean offline;
-    private boolean free;
-    @Enumerated(EnumType.STRING) // 기본값 == ordinal (순서에 따라 0,1,2.. 를 반환)
-    private EventStatus eventStatus = EventStatus.DRAFT;
 
 }
