9da6b6ee6bc3a933f3b80eb85c7bd194e193a434
wwan13 <wwan13@naver.com>
2022-11-12 17:03:23 +0900

    [feat]: initialize project

---
+++ study-spring/src/main/java/com/wwan13/studyspring/StudySpringApplication.java
@@ -0,0 +1,13 @@
+package com.wwan13.studyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySpringApplication.class, args);
+	}
+
+}
