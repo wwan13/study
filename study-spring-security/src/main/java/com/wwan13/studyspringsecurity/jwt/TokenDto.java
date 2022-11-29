56568d0630184f7c837bf21235892a250015515b
wwan13 <wwan13@naver.com>
2022-11-29 21:52:27 +0900

    [feat]: token DTO

---
+++ study-spring-security/src/main/java/com/wwan13/studyspringsecurity/jwt/TokenDto.java
@@ -0,0 +1,17 @@
+package com.wwan13.studyspringsecurity.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String grantType;
+    private String accessToken;
+    private String refreshToken;
+    private Long accessTokenExpiration;
+}
