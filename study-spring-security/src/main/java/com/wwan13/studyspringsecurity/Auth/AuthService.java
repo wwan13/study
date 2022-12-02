c20e30b8d599890730031bc39c87e64c71c96d81
wwan13 <wwan13@naver.com>
2022-12-02 14:03:23 +0900

    [feat]: auth service

--- study-spring-security/src/main/java/com/wwan13/studyspringsecurity/Auth/AuthService.java
+++ study-spring-security/src/main/java/com/wwan13/studyspringsecurity/Auth/AuthService.java
@@ -1,9 +1,15 @@
 package com.wwan13.studyspringsecurity.Auth;
 
+import com.wwan13.studyspringsecurity.User.User;
 import com.wwan13.studyspringsecurity.User.UserRepository;
+import com.wwan13.studyspringsecurity.User.UserRequestDto;
+import com.wwan13.studyspringsecurity.User.UserResponseDto;
+import com.wwan13.studyspringsecurity.jwt.TokenDto;
 import com.wwan13.studyspringsecurity.jwt.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +18,40 @@ public class AuthService {
 
     @Autowired
     AuthenticationManagerBuilder authenticationManagerBuilder;
-
     @Autowired
     UserRepository userRepository;
-
     @Autowired
     PasswordEncoder passwordEncoder;
-
     @Autowired
     TokenProvider tokenProvider;
+
+    public UserResponseDto signup(UserRequestDto userRequestDto) {
+        if (userRepository.existsByUsername(userRequestDto.getUsername())) {
+            throw new RuntimeException("이미 가입된 유저입니다.");
+        }
+
+        User user = userRequestDto.toUser(passwordEncoder);
+        return UserResponseDto.of(user);
+    }
+
+    public TokenDto login(UserRequestDto userRequestDto) {
+
+        // request 의 id, pw를 바탕으로 authentication token 생성
+        UsernamePasswordAuthenticationToken authenticationToken = userRequestDto.toAuthentication();
+
+        // 검증 (비밀번호 체크) 이 이뤄지는 부분
+        // authenticate 메서드가 실행될때 CustomUserDetailService 의 loadUserByUsername 메서드가 실행됨
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 인증 정보로 토큰 생성
+        TokenDto tokenDto = tokenProvider.generateToken(authentication);
+
+        // refresh token 에 대한 로직도 추가 해야함
+
+        return tokenDto;
+
+    }
+
+
 
 }
---
+++ study-spring-security/src/main/java/com/wwan13/studyspringsecurity/User/CustomUserDetailService.java
@@ -0,0 +1,37 @@
+package com.wwan13.studyspringsecurity.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("데이터베이스에서 찾을 수 없습니다."));
+    }
+
+    // DB 에 User 값이 존재한다면 UserDetails 객체로 만들어서 리턴
+    private UserDetails createUserDetails(User user) {
+
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getAuthority().toString());
+
+        return new org.springframework.security.core.userdetails.User(
+                String.valueOf(user.getId()),
+                user.getPassword(),
+                Collections.singleton(grantedAuthority)
+        );
+    }
+
+}
