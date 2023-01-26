13f73d3cd2683246eff60838facef29a45513d53
wwan13 <wwan13@naver.com>
2023-01-26 17:18:09 +0900

    [feat]: account controller signup

--- spring-security-oauth2/src/main/java/com/wwan13/springsecurityoauth2/accounts/AccountController.java
+++ spring-security-oauth2/src/main/java/com/wwan13/springsecurityoauth2/accounts/AccountController.java
@@ -2,13 +2,20 @@ package com.wwan13.springsecurityoauth2.accounts;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value = "/auth")
 public class AccountController {
 
-    private final AccountRepository accountRepository;
+    private final AccountService accountService;
 
-//    @PostMapping()
+    @PostMapping(value = "/signup")
+    public void signup(@RequestBody Account account) {
+        Account newAccount = this.accountService.signup(account);
+
+    }
 }
